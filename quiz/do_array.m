2d3ba76e6419a074d21da1d79b590d896280905a
jhonathan-bc <jonathan@betterchains.com>
2024-12-10 21:17:30 +0200

    fixed quiz1

--- quiz/do_array.m
+++ quiz/do_array.m
@@ -1,10 +1,8 @@
-function [row_oddA,row_evenA] = do_array(row,a)
-row_oddA = nan(1,length(row));
-row_evenA = nan(1,length(row));
-evens = row(2:2:end);
-odds = row(1:2:end);
-row_oddA(2:2:end) = evens;
-row_oddA(1:2:end) = odds.*a;
-row_evenA(1:2:end) = odds;
-row_evenA(2:2:end) = evens.*a;
+function [row_oddA, row_evenA] = do_array(row, a)
+    row_oddA = row;
+    p1 = row(1:2:end) * a;
+    row_oddA(1:2:end) = p1;
+    row_evenA = row;
+    p2 = row(2:2:end) * a;
+    row_evenA(2:2:end) = p2;
 end
--- quiz/quiz1.m
+++ quiz/quiz1.m
@@ -1,12 +1,15 @@
+% clear workspace
 clear
-x= linspace(0,10,51);
-y = x./(x.^2 + 9);
-hold on
-plot(x,y)
-
-[zOdd,zEven] = do_array(y,0.85);
-plot(x,zEven,x,zOdd)
-
-[tOdd] = do_array(zEven,1.25);
-plot(x,tOdd)
-legend("y","zEven","zOdd","tOdd",Location="best")
+% set x as a range of 51 values between 0 and 10
+x = linspace(0, 10, 51);
+% set y as x/(x^2 + 9)
+y = x ./ (x .^ 2 + 9);
+% plot x and y
+plot(x, y);
+% set zODD,zEVEN as the result of do_array applied to y and 0.85
+[zODD, zEVEN] = do_array(y, 0.85);
+% set tOdd as the result of do_array applied to zEVEN and 1.25
+tOdd = do_array(zEVEN, 1.25);
+% plot x and tOdd
+hold on;
+plot(x, tOdd);
