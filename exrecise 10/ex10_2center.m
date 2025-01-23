d7c11974af5f73e03d372d2865031192f32fbe49
jonathan-biu <jonathannaftali.steinmetz@live.biu.ac.il>
2025-01-23 12:53:29 +0200

    fixed ex10

--- exrecise 10/ex10_2center.m
+++ exrecise 10/ex10_2center.m
@@ -8,18 +8,18 @@ load CALMODULIN2D % get data
 % generate backbone
 xyz = d5unite(crdN, crdCA, crdC); % one 2D array of 3 columns
 % save mean of each column xyz as center
-center = mean(xyz, 1);
+Center = mean(xyz, 1);
 hold off
 plot3(xyz(:, 1), xyz(:, 2), xyz(:, 3), 'k')
 % calc dist from center for N
-DistN = ex10_2distances(center, crdN);
-DistCA = ex10_2distances(center, crdCA);
-DistC = ex10_2distances(center, crdC);
+distN = ex10_2distances(Center, crdN);
+distCA = ex10_2distances(Center, crdCA);
+distC = ex10_2distances(Center, crdC);
 % plot each kind of atom in different colors
 hold on
-scatter3(crdN(:, 1), crdN(:, 2), crdN(:, 3), 20, DistN, "o")
-scatter3(crdCA(:, 1), crdCA(:, 2), crdCA(:, 3), 20, DistCA, "square")
-scatter3(crdC(:, 1), crdC(:, 2), crdC(:, 3), 20, DistC,"diamond")
+scatter3(crdN(:, 1), crdN(:, 2), crdN(:, 3), 20, distN, "o")
+scatter3(crdCA(:, 1), crdCA(:, 2), crdCA(:, 3), 20, distCA, "square")
+scatter3(crdC(:, 1), crdC(:, 2), crdC(:, 3), 20, distC,"diamond")
 colorbar;
 colormap('copper')
 axis tight
